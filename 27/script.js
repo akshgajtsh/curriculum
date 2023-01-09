70edbaa644b99b37663e7660134a8e3310d3377c
taka <taka4677@ezweb.ne.jp>
2023-01-09 20:31:56 +0900

    学習

---
+++ 27/script.js
@@ -0,0 +1,31 @@
+// 1.
+var variableNumber = 1234;
+console.log(variableNumber);
+
+// 2.
+var variableText = "Text";
+console.log(variableText);
+
+// 3.
+var variableBoolean = true;
+console.log(variableBoolean);
+
+// 4.
+var additonNumber = 10 + 20;
+console.log(additonNumber);
+
+// 5.
+var subtractionNumber = 20 - 10;
+console.log(subtractionNumber);
+
+// 6.
+var multiplicationNumber = 50 * 2;
+console.log(multiplicationNumber);
+
+// 7.
+var divsionNumber = 20 / 2;
+console.log(divsionNumber);
+
+// 8.
+var combinationNumber = "12" + "34";
+console.log(combinationNumber);
